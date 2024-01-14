1c36512e5034a9373b3e912e6f99e102684805d0
Anthony Rocha <josilton@yahoo.com>
2024-01-14 12:58:50 -0800

    i2c module

--- src/i2c.rs
+++ src/i2c.rs
@@ -1,30 +1,19 @@
 use crate::AardvarkError;
-use crate::AardvarkHandle;
+use aardvark_ffi::AardvarkI2cFlags;
+use aardvark_ffi::AardvarkI2cFlags_AA_I2C_10_BIT_ADDR;
+use aardvark_ffi::AardvarkI2cFlags_AA_I2C_NO_FLAGS;
+use aardvark_ffi::{aa_i2c_read, aa_i2c_write, Aardvark};
 use embedded_hal::i2c::ErrorType;
 use embedded_hal::i2c::{I2c, Operation as I2cOperation, SevenBitAddress, TenBitAddress};
 use std::fmt;
-use std::ops::Deref;
+
 pub struct I2CDevice {
-    handle: AardvarkHandle,
+    handle: Aardvark,
 }
 
 impl I2CDevice {
-    pub fn new(handle: AardvarkHandle) -> Self {
+    pub fn new(handle: Aardvark) -> Self {
         Self { handle }
-    }
-}
-
-impl std::ops::Deref for I2CDevice {
-    type Target = AardvarkHandle;
-
-    fn deref(&self) -> &Self::Target {
-        &self.handle
-    }
-}
-
-impl std::ops::DerefMut for I2CDevice {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.handle
     }
 }
 
@@ -37,14 +26,22 @@ impl I2c<TenBitAddress> for I2CDevice {
         for (_, operation) in operations.iter_mut().enumerate() {
             match operation {
                 I2cOperation::Read(buffer) => {
-                    self.deref()
-                        .aa_i2c_read(address, 0, buffer)
-                        .map_err(|err| I2CError(err))?;
+                    aardvark_i2c_read(
+                        self.handle,
+                        address,
+                        AardvarkI2cFlags_AA_I2C_10_BIT_ADDR,
+                        buffer,
+                    )
+                    .map_err(I2CError)?;
                 }
-                I2cOperation::Write(bytes) => {
-                    self.deref()
-                        .aa_i2c_write(address, 0, bytes)
-                        .map_err(|err| I2CError(err))?;
+                I2cOperation::Write(buffer) => {
+                    aardvark_i2c_write(
+                        self.handle,
+                        address,
+                        AardvarkI2cFlags_AA_I2C_10_BIT_ADDR,
+                        buffer,
+                    )
+                    .map_err(I2CError)?;
                 }
             }
         }
@@ -57,7 +54,29 @@ impl I2c<SevenBitAddress> for I2CDevice {
         address: u8,
         operations: &mut [I2cOperation],
     ) -> Result<(), Self::Error> {
-        I2c::<TenBitAddress>::transaction(self, u16::from(address), operations)
+        for (_, operation) in operations.iter_mut().enumerate() {
+            match operation {
+                I2cOperation::Read(buffer) => {
+                    aardvark_i2c_read(
+                        self.handle,
+                        address as u16,
+                        AardvarkI2cFlags_AA_I2C_NO_FLAGS,
+                        buffer,
+                    )
+                    .map_err(I2CError)?;
+                }
+                I2cOperation::Write(buffer) => {
+                    aardvark_i2c_write(
+                        self.handle,
+                        address as u16,
+                        AardvarkI2cFlags_AA_I2C_NO_FLAGS,
+                        buffer,
+                    )
+                    .map_err(I2CError)?;
+                }
+            }
+        }
+        Ok(())
     }
 }
 #[derive(Debug)]
@@ -79,4 +98,50 @@ impl embedded_hal::i2c::Error for I2CError {
     fn kind(&self) -> embedded_hal::i2c::ErrorKind {
         embedded_hal::i2c::ErrorKind::Other
     }
+}
+
+pub fn aardvark_i2c_read(
+    aardvark: Aardvark,
+    address: u16,
+    flags: AardvarkI2cFlags,
+    buffer: &mut [u8],
+) -> Result<(), AardvarkError> {
+    let status = unsafe {
+        aa_i2c_read(
+            aardvark,
+            address,
+            flags,
+            buffer.len() as u16,
+            buffer.as_mut_ptr(),
+        )
+    };
+
+    if status != 0 {
+        let error = AardvarkError::new(status);
+        return Err(error);
+    }
+    Ok(())
+}
+
+pub fn aardvark_i2c_write(
+    aardvark: Aardvark,
+    address: u16,
+    flags: AardvarkI2cFlags,
+    buffer: &[u8],
+) -> Result<(), AardvarkError> {
+    let status = unsafe {
+        aa_i2c_write(
+            aardvark,
+            address,
+            flags,
+            buffer.len() as u16,
+            buffer.as_ptr(),
+        )
+    };
+
+    if status != 0 {
+        let error = AardvarkError::new(status);
+        return Err(error);
+    }
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,3 @@
-use aardvark_ffi::Aardvark;
-
-#[derive(Clone, Debug)]
-pub struct AardvarkHandle(Aardvark);
-
 pub mod error;
 pub mod i2c;
 
